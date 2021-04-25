3d437f9ad36fbbe13c40dcabdcfbb8f5e347a695
mark <mark@marks-MacBook-Pro.local>
2021-04-25 16:29:58 -0700

    files and folders added

---
+++ models/user.js
@@ -0,0 +1,9 @@
+const {model, DataTypes} = require("sequelize");
+const sequelize = require ("./config/connection");
+class User extends model {
+
+    
+}
+
+
+module.exports = user;
---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require ("express");
+const routes = require ("/routes");
+const Sequelize = require ("sequelize");
+const sequelize = require ("./config/connection");
+const app = express();
+const port = process.env.PORT || 3006;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(routes);
+sequelize.sync ({}).then(() => {
+    console.log ("Sequelize Running");
+    app.listen(port, () => console.log("express connected"))
+});
